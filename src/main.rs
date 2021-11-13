00025ada5bbc03363d7a50db5adb290860be27dc
Sindre Fjermestad <37833410+DataScienceLeadJr@users.noreply.github.com>
2021-11-13 13:55:45 +0100

    first

---
+++ src/main.rs
@@ -0,0 +1,9 @@
+fn main() {
+    println!("Hello, world!");
+
+    //TODO: "module" wrapper to make main call different sketches/apps
+    //TODO: make a module init from arg part.
+    //TODO: include the viz-nannou stuff into this structure
+    //TODO: make a lib for the different drawing parts that can be made into "brushes"
+    //TODO: include the nokwha camera as a thing to be usable.
+}
